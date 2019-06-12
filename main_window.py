661f430c171d67890a29741424b754b8fb6fdaf2
carlprilo <chenpan19971211@126.com>
2019-06-12 21:16:53 +0800

    add two cameras

--- main_window.py
+++ main_window.py
@@ -7,7 +7,8 @@ Last edited: 21 Feb 2018
 
 # import system module
 import sys
-
+#import os module
+import os
 # import some PyQt5 modules
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QWidget
@@ -35,10 +36,22 @@ class MainWindow(QWidget):
         # set control_bt callback clicked  function
         self.ui.control_bt.clicked.connect(self.controlTimer)
 
+        self.lastImage0 = ""
+        self.lastImage1 = ""
+        self.checkSavePath()
+
+    def checkSavePath(self):
+        pathList = ('./normal','./abnormal1','./abnormal2')
+        for i in pathList:
+            if not os.path.exists(i):
+                os.makedirs(i)
+
     # view camera
     def viewCam(self):
         # read image in BGR format
-        ret, image = self.cap.read()
+        ret, image = self.cap0.read()
+        #collect image
+        self.lastImage0 = image
         # convert image to RGB format
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         # get image infos
@@ -47,14 +60,30 @@ class MainWindow(QWidget):
         # create QImage from image
         qImg = QImage(image.data, width, height, step, QImage.Format_RGB888)
         # show image in img_label
-        self.ui.image_label.setPixmap(QPixmap.fromImage(qImg))
+        self.ui.image_label0.setPixmap(QPixmap.fromImage(qImg))
+
+                # read image in BGR format
+        ret, image = self.cap1.read()
+        #collect image
+        self.lastImage1 = image
+        # convert image to RGB format
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # get image infos
+        height, width, channel = image.shape
+        step = channel * width
+        # create QImage from image
+        qImg = QImage(image.data, width, height, step, QImage.Format_RGB888)
+        # show image in img_label
+        self.ui.image_label1.setPixmap(QPixmap.fromImage(qImg))
+
 
     # start/stop timer
     def controlTimer(self):
         # if timer is stopped
         if not self.timer.isActive():
             # create video capture
-            self.cap = cv2.VideoCapture(0)
+            self.cap0 = cv2.VideoCapture(0)
+            self.cap1 = cv2.VideoCapture(1)
             # start timer
             self.timer.start(20)
             # update control_bt text
@@ -64,10 +93,16 @@ class MainWindow(QWidget):
             # stop timer
             self.timer.stop()
             # release video capture
-            self.cap.release()
+            self.cap0.release()
+            self.cap1.release()
             # update control_bt text
             self.ui.control_bt.setText("Start")
 
+            self.saveImage()
+
+    def saveImage(self):
+        cv2.imwrite("test0.png",self.lastImage0)
+        cv2.imwrite("test1.png",self.lastImage1)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- ui_main_window.py
+++ ui_main_window.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'main_window.ui'
+# Form implementation generated from reading ui file 'ui_main_window.ui'
 #
-# Created by: PyQt5 UI code generator 5.5.1
+# Created by: PyQt5 UI code generator 5.12.2
 #
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+
 
 class Ui_Form(object):
     def setupUi(self, Form):
@@ -16,13 +17,40 @@ class Ui_Form(object):
         self.horizontalLayout.setObjectName("horizontalLayout")
         self.verticalLayout = QtWidgets.QVBoxLayout()
         self.verticalLayout.setObjectName("verticalLayout")
-        self.image_label = QtWidgets.QLabel(Form)
-        self.image_label.setObjectName("image_label")
-        self.verticalLayout.addWidget(self.image_label)
         self.control_bt = QtWidgets.QPushButton(Form)
         self.control_bt.setObjectName("control_bt")
         self.verticalLayout.addWidget(self.control_bt)
         self.horizontalLayout.addLayout(self.verticalLayout)
+        self.verticalLayout0 = QtWidgets.QVBoxLayout()
+        self.verticalLayout0.setObjectName("verticalLayout0")
+        self.image_label0 = QtWidgets.QLabel(Form)
+        self.image_label0.setObjectName("image_label0")
+        self.verticalLayout0.addWidget(self.image_label0)
+        self.dirs_box0 = QtWidgets.QComboBox(Form)
+        self.dirs_box0.setObjectName("dirs_box0")
+        self.verticalLayout0.addWidget(self.dirs_box0)
+        self.initBox(self.dirs_box0)
+        self.horizontalLayout.addLayout(self.verticalLayout0)
+        self.verticalLayout1 = QtWidgets.QVBoxLayout()
+        self.verticalLayout1.setObjectName("verticalLayout1")
+        self.image_label1 = QtWidgets.QLabel(Form)
+        self.image_label1.setObjectName("image_label1")
+        self.verticalLayout1.addWidget(self.image_label1)
+        self.dirs_box1 = QtWidgets.QComboBox(Form)
+        self.dirs_box1.setObjectName("dirs_box1")
+        self.initBox(self.dirs_box1)
+        self.verticalLayout1.addWidget(self.dirs_box1)
+        self.horizontalLayout.addLayout(self.verticalLayout1)
+        self.verticalLayout2 = QtWidgets.QVBoxLayout()
+        self.verticalLayout2.setObjectName("verticalLayout2")
+        self.image_label2 = QtWidgets.QLabel(Form)
+        self.image_label2.setObjectName("image_label2")
+        self.verticalLayout2.addWidget(self.image_label2)
+        self.dirs_box2 = QtWidgets.QComboBox(Form)
+        self.dirs_box2.setObjectName("dirs_box2")
+        self.initBox(self.dirs_box2)
+        self.verticalLayout2.addWidget(self.dirs_box2)
+        self.horizontalLayout.addLayout(self.verticalLayout2)
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
@@ -30,5 +58,17 @@ class Ui_Form(object):
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("Form", "Cam view"))
-        self.image_label.setText(_translate("Form", "TextLabel"))
         self.control_bt.setText(_translate("Form", "Start"))
+        self.image_label0.setText(_translate("Form", "TextLabel"))
+
+        self.image_label1.setText(_translate("Form", "TextLabel"))
+
+        self.image_label2.setText(_translate("Form", "TextLabel"))
+
+
+    def initBox(self,box):
+        box.addItem("normal")
+        box.addItem("abnormal1")
+        box.addItem("abnormal2")
+
+
