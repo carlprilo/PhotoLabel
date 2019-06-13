a2a38cdce998ef6165becf3654a5317ad1c53c2a
carlprilo <chenpan19971211@126.com>
2019-06-13 12:28:27 +0800

    version 1.0

--- main_window.py
+++ main_window.py
@@ -1,15 +1,12 @@
-"""
-In this example, we demonstrate how to create simple camera viewer using Opencv3 and PyQt5
-
-Author: Berrouba.A
-Last edited: 21 Feb 2018
-"""
 
 # import system module
 import sys
 #import os module
 import os
 # import some PyQt5 modules
+import time
+import datetime
+
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QWidget
 from PyQt5.QtGui import QImage
@@ -26,64 +23,58 @@ class MainWindow(QWidget):
     def __init__(self):
         # call QWidget constructor
         super().__init__()
-        self.ui = Ui_Form()
+        self.boxContent = ["normal","abnormal1","abnormal2"]
+        self.cameraNum = 2
+        self.ui = Ui_Form(self.cameraNum,self.boxContent)
         self.ui.setupUi(self)
-
         # create a timer
         self.timer = QTimer()
         # set timer timeout callback function
         self.timer.timeout.connect(self.viewCam)
         # set control_bt callback clicked  function
         self.ui.control_bt.clicked.connect(self.controlTimer)
-
-        self.lastImage0 = ""
-        self.lastImage1 = ""
+        self.ui.save_bt.clicked.connect(self.saveImage)
+        self.lastImageList = ["",""]
         self.checkSavePath()
+        self.capList = []
+        self.time = int(round(time.time()*1000))
 
     def checkSavePath(self):
-        pathList = ('./normal','./abnormal1','./abnormal2')
-        for i in pathList:
+        for i in self.boxContent:
             if not os.path.exists(i):
                 os.makedirs(i)
 
     # view camera
     def viewCam(self):
-        # read image in BGR format
-        ret, image = self.cap0.read()
-        #collect image
-        self.lastImage0 = image
-        # convert image to RGB format
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        # get image infos
-        height, width, channel = image.shape
-        step = channel * width
-        # create QImage from image
-        qImg = QImage(image.data, width, height, step, QImage.Format_RGB888)
-        # show image in img_label
-        self.ui.image_label0.setPixmap(QPixmap.fromImage(qImg))
-
-                # read image in BGR format
-        ret, image = self.cap1.read()
-        #collect image
-        self.lastImage1 = image
-        # convert image to RGB format
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        # get image infos
-        height, width, channel = image.shape
-        step = channel * width
-        # create QImage from image
-        qImg = QImage(image.data, width, height, step, QImage.Format_RGB888)
-        # show image in img_label
-        self.ui.image_label1.setPixmap(QPixmap.fromImage(qImg))
-
+        for i in range(self.cameraNum):
+            # read image in BGR format
+            ret, image = self.capList[i].read()
+            #collect image
+            self.lastImageList[i] = image
+            self.time = int(round(time.time()*1000))
+            # convert image to RGB format
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            # get image infos
+            height, width, channel = image.shape
+            step = channel * width
+            # create QImage from image
+            qImg = QImage(image.data, width, height, step, QImage.Format_RGB888)
+            # show image in img_label
+            self.ui.imageLabelList[i].setPixmap(QPixmap.fromImage(qImg))
 
     # start/stop timer
     def controlTimer(self):
         # if timer is stopped
         if not self.timer.isActive():
             # create video capture
-            self.cap0 = cv2.VideoCapture(0)
-            self.cap1 = cv2.VideoCapture(1)
+            if len(self.capList) == 0:
+                for i in range(self.cameraNum):
+                    print(i)
+                    cap = cv2.VideoCapture(i)
+                    self.capList.append(cap)
+            else:
+                for i in range(self.cameraNum):
+                    self.capList[i] = cv2.VideoCapture(i)
             # start timer
             self.timer.start(20)
             # update control_bt text
@@ -93,17 +84,17 @@ class MainWindow(QWidget):
             # stop timer
             self.timer.stop()
             # release video capture
-            self.cap0.release()
-            self.cap1.release()
+            for cap in self.capList:
+                cap.release()
+            
             # update control_bt text
             self.ui.control_bt.setText("Start")
 
-            self.saveImage()
-
     def saveImage(self):
-        cv2.imwrite("test0.png",self.lastImage0)
-        cv2.imwrite("test1.png",self.lastImage1)
-
+        for i in range(self.cameraNum):
+            boxText = self.ui.boxList[i].currentText()
+            cv2.imwrite("%s/%d-%d.png"%(boxText,self.time,i),self.lastImageList[i])
+        
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
--- ui_main_window.py
+++ ui_main_window.py
@@ -10,6 +10,14 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 
 class Ui_Form(object):
+    def __init__(self,num,content):
+        # call QWidget constructor
+        super().__init__()
+        self.boxList = []
+        self.cameraNum = num
+        self.imageLabelList = []
+        self.verLayoutList = []
+        self.boxContent = content
     def setupUi(self, Form):
         Form.setObjectName("Form")
         Form.resize(525, 386)
@@ -20,37 +28,26 @@ class Ui_Form(object):
         self.control_bt = QtWidgets.QPushButton(Form)
         self.control_bt.setObjectName("control_bt")
         self.verticalLayout.addWidget(self.control_bt)
+        self.save_bt = QtWidgets.QPushButton(Form)
+        self.save_bt.setObjectName("save_bt")
+        self.verticalLayout.addWidget(self.save_bt)
         self.horizontalLayout.addLayout(self.verticalLayout)
-        self.verticalLayout0 = QtWidgets.QVBoxLayout()
-        self.verticalLayout0.setObjectName("verticalLayout0")
-        self.image_label0 = QtWidgets.QLabel(Form)
-        self.image_label0.setObjectName("image_label0")
-        self.verticalLayout0.addWidget(self.image_label0)
-        self.dirs_box0 = QtWidgets.QComboBox(Form)
-        self.dirs_box0.setObjectName("dirs_box0")
-        self.verticalLayout0.addWidget(self.dirs_box0)
-        self.initBox(self.dirs_box0)
-        self.horizontalLayout.addLayout(self.verticalLayout0)
-        self.verticalLayout1 = QtWidgets.QVBoxLayout()
-        self.verticalLayout1.setObjectName("verticalLayout1")
-        self.image_label1 = QtWidgets.QLabel(Form)
-        self.image_label1.setObjectName("image_label1")
-        self.verticalLayout1.addWidget(self.image_label1)
-        self.dirs_box1 = QtWidgets.QComboBox(Form)
-        self.dirs_box1.setObjectName("dirs_box1")
-        self.initBox(self.dirs_box1)
-        self.verticalLayout1.addWidget(self.dirs_box1)
-        self.horizontalLayout.addLayout(self.verticalLayout1)
-        self.verticalLayout2 = QtWidgets.QVBoxLayout()
-        self.verticalLayout2.setObjectName("verticalLayout2")
-        self.image_label2 = QtWidgets.QLabel(Form)
-        self.image_label2.setObjectName("image_label2")
-        self.verticalLayout2.addWidget(self.image_label2)
-        self.dirs_box2 = QtWidgets.QComboBox(Form)
-        self.dirs_box2.setObjectName("dirs_box2")
-        self.initBox(self.dirs_box2)
-        self.verticalLayout2.addWidget(self.dirs_box2)
-        self.horizontalLayout.addLayout(self.verticalLayout2)
+        
+        
+        for i in range (self.cameraNum):
+            verticalLayout = QtWidgets.QVBoxLayout()
+            verticalLayout.setObjectName("%s%d"%("verticalLayout",i))
+            image_label = QtWidgets.QLabel(Form)
+            image_label.setObjectName("%s%d"%("image_label",i))
+            verticalLayout.addWidget(image_label)
+            dirs_box = QtWidgets.QComboBox(Form)
+            dirs_box.setObjectName("%s%d"%("dirs_box",i))
+            self.initBox(dirs_box)
+            verticalLayout.addWidget(dirs_box)
+            self.horizontalLayout.addLayout(verticalLayout)
+            self.verLayoutList.append(verticalLayout)
+            self.imageLabelList.append(image_label)
+            self.boxList.append(dirs_box)
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
@@ -59,16 +56,10 @@ class Ui_Form(object):
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("Form", "Cam view"))
         self.control_bt.setText(_translate("Form", "Start"))
-        self.image_label0.setText(_translate("Form", "TextLabel"))
-
-        self.image_label1.setText(_translate("Form", "TextLabel"))
-
-        self.image_label2.setText(_translate("Form", "TextLabel"))
-
+        self.save_bt.setText(_translate("Form","Save"))
+        for image_label  in self.imageLabelList:
+            image_label.setText(_translate("Form", "Camera"))
 
     def initBox(self,box):
-        box.addItem("normal")
-        box.addItem("abnormal1")
-        box.addItem("abnormal2")
-
-
+        for i in self.boxContent:
+            box.addItem(i)
